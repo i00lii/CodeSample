88d4375623bfc076a586bb283bb72f11842d2567
Daniil Dyachenko <dydaniil@yandex.ru>
2023-01-17 20:19:09 +0400

    Added collection consumer

---
+++ Consumers/CollectionConsumer.cs
@@ -0,0 +1,23 @@
+﻿namespace CodeSample;
+
+/// <summary>
+/// The <see cref="IConsumer{TSignal, TResult}"/> implementation 
+/// which aggregates the <see cref="TSignal"/> sequence into the <see cref="List{TSignal}"/> instance.
+/// </summary>
+internal class CollectionConsumer<TSignal> : IConsumer<TSignal, List<TSignal>>
+{
+    private readonly AggregareConsumer<TSignal, List<TSignal>> _inner;
+
+    public CollectionConsumer() 
+        => _inner = new AggregareConsumer<TSignal, List<TSignal>>
+        (
+            (current, aggregate) => 
+            { 
+                aggregate.Add(current);
+                return aggregate;
+            },
+            () => new List<TSignal>()
+        );
+
+    public Task<List<TSignal>> Consume(IReceiver<TSignal> receiver) => _inner.Consume(receiver);
+}
