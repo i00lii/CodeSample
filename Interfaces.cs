d6d43c752eafc135c80fa5bcafecc3a8e72024d8
Daniil Dyachenko <dydaniil@yandex.ru>
2023-01-17 20:18:35 +0400

    Added IConsumer interface

--- Interfaces.cs
+++ Interfaces.cs
@@ -11,3 +11,17 @@ public interface IReceiver<TSignal>
     /// <returns>The asynchronous <see cref="TSignal"/> sequence.</returns>
     IAsyncEnumerable<TSignal> Connect(CancellationToken cancellationToken = default);
 }
+
+/// <summary>
+/// Represents the very common interface for <see cref="TSignal"/> consuming.
+/// </summary>
+public interface IConsumer<TSignal, TResult>
+{
+    /// <summary>
+    /// Consumes the whole <see cref="TSignal"/> sequence from the <paramref name="receiver"/> 
+    /// and returns the <see cref="TResult"/> aggregation.
+    /// </summary>
+    /// <param name="receiver">The asynchronous <see cref="TSignal"/> sequence provider.</param>
+    /// <returns>The aggregation of the <see cref="TSignal"/> sequence.</returns>
+    Task<TResult> Consume(IReceiver<TSignal> receiver);
+}
