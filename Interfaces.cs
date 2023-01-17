a0654e4d9ccf34de8588a6909dc724e42816cca2
Daniil Dyachenko <dydaniil@yandex.ru>
2023-01-17 20:14:43 +0400

    Added IReceiver interface

---
+++ Interfaces.cs
@@ -0,0 +1,13 @@
+﻿namespace CodeSample;
+
+/// <summary>
+/// Represents the very common interface of <see cref="TSignal"/> receiver.
+/// </summary>
+public interface IReceiver<TSignal>
+{
+    /// <summary>
+    /// Opens the channel and starts to recieve <see cref="TSignal"/> sequence.
+    /// </summary>
+    /// <returns>The asynchronous <see cref="TSignal"/> sequence.</returns>
+    IAsyncEnumerable<TSignal> Connect(CancellationToken cancellationToken = default);
+}
