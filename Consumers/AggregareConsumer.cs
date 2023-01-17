ace7cd6ec22431892bc438adefc3253be19cb5e9
Daniil Dyachenko <dydaniil@yandex.ru>
2023-01-17 20:18:51 +0400

    Added aggregate consumer

---
+++ Consumers/AggregareConsumer.cs
@@ -0,0 +1,30 @@
+﻿namespace CodeSample;
+
+/// <summary>
+/// The <see cref="IConsumer{TSignal, TResult}"/> implementation 
+/// which aggregates the <see cref="TSignal"/> sequence into single <see cref="TResult"/> instance.
+/// </summary>
+public class AggregareConsumer<TSignal, TResult> : IConsumer<TSignal, TResult>
+{
+    private readonly Func<TSignal, TResult, TResult> _aggregateLambda;
+    private readonly Func<TResult> _initialValueFactory;
+
+    public AggregareConsumer(Func<TSignal, TResult, TResult> aggregate, Func<TResult> initialValueFactory)
+    {
+        _aggregateLambda = aggregate;
+        _initialValueFactory = initialValueFactory;
+    }
+
+    /// <inheritdoc />
+    public async Task<TResult> Consume(IReceiver<TSignal> receiver)
+    {
+        TResult result = _initialValueFactory();
+
+        await foreach(TSignal signal in receiver.Connect())
+        {
+            result = _aggregateLambda(signal, result);
+        }
+
+        return result;
+    }
+}
