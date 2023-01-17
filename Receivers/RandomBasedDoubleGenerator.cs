28ba9c71cb3bd850532f37fcf262c030fd870dac
Daniil Dyachenko <dydaniil@yandex.ru>
2023-01-17 20:15:28 +0400

    Added a couple of receiver implementations

---
+++ Receivers/RandomBasedDoubleGenerator.cs
@@ -0,0 +1,23 @@
+﻿namespace CodeSample;
+
+/// <summary>
+/// Provides <see cref="IReceiver{double}"/> based on <see cref="Random"/>.
+/// </summary>
+public sealed class RandomBasedDoubleGenerator : IReceiver<double>
+{
+    private readonly Random _random;
+    private readonly int _count;
+
+    public RandomBasedDoubleGenerator(Random random, int count) => (_random, _count) = (random, count);
+    
+    /// <inheritdoc />
+    public IAsyncEnumerable<double> Connect(CancellationToken cancellationToken = default) => Yield().AsAsync();
+
+    private IEnumerable<double> Yield()
+    {
+        foreach (int _ in Enumerable.Range(0, _count))
+        {
+            yield return _random.NextDouble();
+        }
+    }
+}
---
+++ Receivers/RestApiBasedInt64Generator.cs
@@ -0,0 +1,51 @@
+﻿using System.Buffers;
+using System.Runtime.CompilerServices;
+
+namespace CodeSample;
+
+/// <summary>
+/// Provides <see cref="IReceiver{long}"/> based on API provided by 'httpbin.org'.
+/// </summary>
+public sealed class RestApiBasedInt64Generator : IReceiver<long>
+{
+    private const int _byteCount = sizeof(long);
+
+    private readonly HttpClient _http;
+    private readonly int _count;
+    private readonly Uri _url;
+
+    public RestApiBasedInt64Generator(HttpClient http, int count)
+    {
+        _http = http;
+        _count = count;
+        _url = new Uri($"https://httpbin.org/bytes/{_byteCount}");
+    }
+
+    /// <inheritdoc />
+    public async IAsyncEnumerable<long> Connect([EnumeratorCancellation] CancellationToken cancellation = default)
+    {
+        foreach (int _ in Enumerable.Range(0, _count))
+        {
+            yield return await InvokeRequest(cancellation);
+        }
+    }
+
+    private async Task<long> InvokeRequest(CancellationToken cancellationToken)
+    {
+        using HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, _url);
+        using HttpResponseMessage response = await _http.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
+        using Stream content = await response.Content.ReadAsStreamAsync(cancellationToken);
+
+        byte[] buffer = ArrayPool<byte>.Shared.Rent(_byteCount);
+
+        try
+        {
+            await content.ReadExactlyAsync(buffer, 0, _byteCount, cancellationToken);
+            return BitConverter.ToInt64(buffer, 0);
+        }
+        finally
+        {
+            ArrayPool<byte>.Shared.Return(buffer);
+        }
+    }
+}
